f789deecb0a345404c403b36e589d8a8c94b1180
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2019-06-06 15:16:19 -0400

    Add gateway url

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.get('/', async (req, res, next) => {
-  const env = process.env.NODE_ENV
-  res.status(200).send(`Hello World! We're in ${env}!`)
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  const GATEWAY_URL = process.env.GATEWAY_URL || 'localhost:3000'
+  response = {
+    today: new Date().toDateString(),
+    devEnv: process.env.NODE_ENV,
+    region: process.env.REGION,
+    stage: process.env.STAGE,
+    detailUrl: `${GATEWAY_URL}/some-id`
+  }
+  res.status(200).send(response)
+})
+
+app.get('/some-id', async (req, res) => {
+  res.status(200).send('Detail View')
 })
 
 module.exports = app
